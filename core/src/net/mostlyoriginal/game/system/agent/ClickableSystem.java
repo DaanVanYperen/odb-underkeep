6833db50f7b2b132b740a2d7d35e26542d79cc4a
Daan <daan@mostlyoriginal.net>
2014-04-27 11:06:41 +0200

    Made clickables depth sorted.

--- core/src/net/mostlyoriginal/game/system/agent/ClickableSystem.java
+++ core/src/net/mostlyoriginal/game/system/agent/ClickableSystem.java
@@ -3,31 +3,42 @@ package net.mostlyoriginal.game.system.agent;
 import com.artemis.Aspect;
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
+import com.artemis.EntitySystem;
 import com.artemis.annotations.Wire;
 import com.artemis.managers.TagManager;
-import com.artemis.systems.EntityProcessingSystem;
+import com.artemis.utils.ImmutableBag;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import net.mostlyoriginal.api.component.basic.Bounds;
 import net.mostlyoriginal.api.component.basic.Pos;
+import net.mostlyoriginal.api.component.graphics.Anim;
 import net.mostlyoriginal.api.system.physics.CollisionSystem;
 import net.mostlyoriginal.game.component.agent.Clickable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author Daan van Yperen
  */
 @Wire
-public class ClickableSystem extends EntityProcessingSystem {
+public class ClickableSystem extends EntitySystem {
 
     ComponentMapper<Clickable> cm;
+    private ComponentMapper<Anim> sm;
     CollisionSystem collisionSystem;
     TagManager tagManager;
     private Entity mouse;
     private boolean leftButtonDown;
     private boolean found;
 
+    private final List<Entity> sortedEntities = new ArrayList<Entity>();
+    public boolean sortedDirty = false;
+
     public ClickableSystem() {
-        super(Aspect.getAspectForAll(Clickable.class, Bounds.class, Pos.class));
+        super(Aspect.getAspectForAll(Clickable.class, Bounds.class, Pos.class, Anim.class));
     }
 
     @Override
@@ -37,11 +48,48 @@ public class ClickableSystem extends EntityProcessingSystem {
         found=false;
     }
 
+    // flipped compared to rendering, for clickables we want to hit the entity in front first in the list.
+    public Comparator<Entity> layerSortComperator = new Comparator<Entity>() {
+        @Override
+        public int compare(Entity e1, Entity e2) {
+            return sm.get(e2).layer - sm.get(e1).layer;
+        }
+    };
+
     @Override
+    protected void processEntities(ImmutableBag<Entity> entities) {
+
+        if (sortedDirty) {
+            sortedDirty = false;
+            Collections.sort(sortedEntities, layerSortComperator);
+        }
+
+        for (Entity entity : sortedEntities) {
+            process(entity);
+        }
+    }
+
+    @Override
+    protected boolean checkProcessing() {
+        return true;
+    }
+
+
     protected void process(Entity e) {
         Clickable clickable = cm.get(e);
         if ( !found && (leftButtonDown && collisionSystem.overlaps(mouse, e)) ) {
             found = clickable.clicked = true;
         } else clickable.clicked=false;
+    }
+
+    @Override
+    protected void inserted(Entity e) {
+        sortedEntities.add(e);
+        sortedDirty = true;
+    }
+
+    @Override
+    protected void removed(Entity e) {
+        sortedEntities.remove(e);
     }
 }
